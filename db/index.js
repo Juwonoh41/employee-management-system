fc8d81787f200e7e33fc6c452d73458bac7a2a77
Juwonoh41 <edmaster41@gmail.com>
2022-02-12 19:31:29 -0800

    functions

--- db/index.js
+++ db/index.js
@@ -10,10 +10,17 @@ class employee_management{
         return this.route.promise().query(
             "SELECT department.id, department.name FROM department;" );
         }
+    addDept(department){
+        return this.route.promise().query('INSERT into deptartment SET ?', department)
 
+    }
     //roles
     allRoles(){
         return this.route.promise().query('SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;')
+    }
+
+    addRole(role){
+        return this.connection.promise().query("INSERT INTO role SET ?", role);
     }
 
     //employees
@@ -21,6 +28,15 @@ class employee_management{
         return  this.route.promise().query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         )
     }
+    
+    addEmployee(){
+        return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+    }
+
+    updateEmployee(){
+        return this.connection.promise().query( "UPDATE employee SET role_id = ? WHERE id = ?",[roleId, employeeId])
+    }
+
 
 
 }
