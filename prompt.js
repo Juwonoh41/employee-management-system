2d9dba5e19f281bfb86a483dd9b5e193b67b036e
Juwonoh41 <edmaster41@gmail.com>
2022-02-15 14:50:49 -0800

    employee fin

--- prompt.js
+++ prompt.js
@@ -116,8 +116,8 @@ function seeRoles(){
 function createRoles(){
     db.allDepartments().then(([tea])=> {
         let dept = tea
-        const deptOptions = dept.map(({id, name}) => ({
-            name: name,
+        const deptOptions = dept.map(({id, title}) => ({
+            name: title,
             value: id
         }))
         prompt([
@@ -138,7 +138,7 @@ function createRoles(){
         ]).then( pos => {
             db.addRole(pos).then(() => {
                 console.log('\n')
-                console.log(`${pos.position} added.`)
+                console.log(`${pos.title} added.`)
             }).then(() => questions())
         })
     })
@@ -153,11 +153,102 @@ function seeEmployees(){
 }
 
 function createEmployee(){
+    prompt([
+        {
+            name: "FirstName",
+            message: "Enter the first name."
+        },
+        {
+            name: "LastName",
+            message: "Enter the last name"
 
+        }
+    ]).then(res => {
+        let firstName = res.FirstName
+        let lastName = res.LastName
+        db.allRoles().then(([tea]) => {
+            let roles = tea
+            const roleOptions = roles.map(({id, title}) => ({
+                name: title,
+                value: id
+            }))
+
+            prompt(
+                {
+                    type: 'list', 
+                    name: 'roleTitle',
+                    message: 'Employees role',
+                    choices: roleOptions
+                }).then(res => {
+                    let roleId = res.roleId
+                    db.allEmployees().then(([tea]) => {
+                       let employee = tea 
+                       const employeeOptions = employee.map(({ id, first_name, last_name }) => ({
+                            name: `${first_name} ${last_name}`,
+                            value: id
+                       }))
+                       employeeOptions.unshift({ name: "None", value: null })
+                       prompt({
+                        type: "list",
+                        name: "managerId",
+                        message: "Select the Manager Id",
+                        choices: employeeOptions
+                      }).then(res => {
+                        let employee = {
+                          manager_id: res.managerId,
+                          role_id: roleId,
+                          first_name: firstName,
+                          last_name: lastName
+                        }
+                        db.addEmployee(employee);
+                      }).then(() => {
+                          console.log(`${firstName} ${lastName} added.`)
+
+                      }).then(() => questions())
+                    })
+                })
+        })
+    })
 }
 
 function putEmployee(){
+    db.allEmployees()
+    .then(([tea]) => {
+      let employees = tea;
+      const employeeOptions = employees.map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id
+      }));
 
+      prompt([
+        {
+          type: "list",
+          name: "eId",
+          message: "Select the id for the employee you wish to update",
+          choices: employeeOptions
+        }
+      ]).then(res => {
+          let employeeId = res.employeeId;
+          db.allRoles()
+            .then(([tea]) => {
+              let roles = tea;
+              const roleOptions = roles.map(({ id, title }) => ({
+                name: title,
+                value: id
+              }));
+
+              prompt([
+                {
+                  type: "list",
+                  name: "rId",
+                  message: "Select the role",
+                  choices: roleOptions
+                }
+              ]).then(res => db.updateEmployee(eId, res.rId)).then(() => console.log("Employee role figured out"))
+                .then(() => questions())
+            });
+        });
+    })
 }
 
 
